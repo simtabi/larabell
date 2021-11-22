2096d02ccae6ef0dd198486c36dcdba3073ec4a5
Imani Manyara <imani@simtabi.com>
2021-11-22 16:31:39 -0500

    Bug fixes

--- src/Flash/Providers/FlashServiceProvider.php
+++ src/Flash/Providers/FlashServiceProvider.php
@@ -2,22 +2,24 @@
 
 namespace Simtabi\Larabell\Flash\Providers;
 
+use Simtabi\Larabell\Flash\Contracts\FlashMessageFactoryContract;
+use Simtabi\Larabell\Flash\Contracts\FlashMessageRendererContract;
+use Simtabi\Larabell\Flash\Contracts\FlashPreparerContract;
+use Simtabi\Larabell\Flash\Contracts\FlashRendererContract;
+use Simtabi\Larabell\Flash\Contracts\StorageContract;
 use Simtabi\Larabell\Larabell;
 use Simtabi\Larabell\Flash\Message\FlashMessageFactory;
-use Simtabi\Larabell\Flash\Message\FlashMessageFactoryContract;
-use Simtabi\Larabell\Flash\Message\FlashMessageRendererContract;
 use Simtabi\Larabell\Flash\Message\FlashMessageViewRenderer;
 use Simtabi\Larabell\Flash\Flash\Flash;
 use Simtabi\Larabell\Flash\Flash\FlashPreparer;
-use Simtabi\Larabell\Flash\Flash\FlashPreparerContract;
 use Simtabi\Larabell\Flash\Flash\FlashRenderer;
-use Simtabi\Larabell\Flash\Flash\FlashRendererContract;
-use Simtabi\Larabell\Flash\Storage\StorageContract;
 use Simtabi\Larabell\Flash\Storage\StorageManager;
 use Simtabi\Larabell\Flash\Storage\SessionStorage;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\View as ViewFacade;
+use Illuminate\View\View;
 use Illuminate\Support\ServiceProvider;
+use Simtabi\Laratoast\Services\FlashViewMessageBag;
 
 class FlashServiceProvider extends ServiceProvider
 {
@@ -69,48 +71,48 @@ class FlashServiceProvider extends ServiceProvider
         $this->app->booted(function () {
             // This is used when adding a message from a controller: view('posts-index')->withMessage(...)
             View::macro('withMessage', function (Message $message, string $bag = 'default'): View {
-                /** @var ViewFlashMessageBag $viewFlashMessageBag */
-                $viewFlashMessageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new ViewFlashMessageBag());
+                /** @var FlashViewMessageBag $messageBag */
+                $messageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new FlashViewMessageBag());
 
-                ViewFacade::share(Larabell::getConfig('view_share'), $viewFlashMessageBag->push($message, $bag));
+                ViewFacade::share(Larabell::getConfig('view_share'), $messageBag->push($message, $bag));
 
                 return $this;
             });
 
             // This is used when adding a message from the View Facade: ViewFacade::withMessage(...)
             Factory::macro('withMessage', function (Message $message, string $bag = 'default'): Factory {
-                /** @var ViewFlashMessageBag $viewFlashMessageBag */
-                $viewFlashMessageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new ViewFlashMessageBag());
+                /** @var FlashViewMessageBag $messageBag */
+                $messageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new FlashViewMessageBag());
 
-                ViewFacade::share(Larabell::getConfig('view_share'), $viewFlashMessageBag->push($message, $bag));
+                ViewFacade::share(Larabell::getConfig('view_share'), $messageBag->push($message, $bag));
 
                 return $this;
             });
 
             // This is used when adding messages from a controller: view('posts-index')->withMessages(...)
             View::macro('withMessages', function (array $messages, string $bag = 'default'): View {
-                /** @var ViewFlashMessageBag $viewFlashMessageBag */
-                $viewFlashMessageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new ViewFlashMessageBag());
+                /** @var FlashViewMessageBag $messageBag */
+                $messageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new FlashViewMessageBag());
 
                 /** @var Message $message */
                 foreach ($messages as $message) {
-                    $viewFlashMessageBag->push($message, $bag);
+                    $messageBag->push($message, $bag);
                 }
-                ViewFacade::share(Larabell::getConfig('view_share'), $viewFlashMessageBag);
+                ViewFacade::share(Larabell::getConfig('view_share'), $messageBag);
 
                 return $this;
             });
 
             // This is used when adding messages from the View Facade: ViewFacade::withMessages(...)
             Factory::macro('withMessages', function (array $messages, string $bag = 'default'): Factory {
-                /** @var ViewFlashMessageBag $viewFlashMessageBag */
-                $viewFlashMessageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new ViewFlashMessageBag());
+                /** @var FlashViewMessageBag $messageBag */
+                $messageBag = ViewFacade::shared(Larabell::getConfig('view_share'), new FlashViewMessageBag());
 
                 /** @var Message $message */
                 foreach ($messages as $message) {
-                    $viewFlashMessageBag->push($message, $bag);
+                    $messageBag->push($message, $bag);
                 }
-                ViewFacade::share(Larabell::getConfig('view_share'), $viewFlashMessageBag);
+                ViewFacade::share(Larabell::getConfig('view_share'), $messageBag);
 
                 return $this;
             });
--- src/LarabellMiddleware.php
+++ src/LarabellMiddleware.php
@@ -3,24 +3,36 @@
 namespace Simtabi\Larabell;
 
 use Closure;
-use Simtabi\Larabell\Flash\Flash\FlashPreparerContract;
 use Illuminate\Container\Container;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Simtabi\Larabell\Flash\Contracts\FlashPreparerContract;
+use Simtabi\Laratoast\Services\FlashViewMessageBag;
 
 class LarabellMiddleware
 {
+    /**
+     * The view factory implementation.
+     *
+     * @var ViewFactory
+     */
+    protected ViewFactory $view;
+
     protected $larabell;
 
     protected $flash;
 
-    public function __construct(FlashPreparerContract $flash)
+    public function __construct(FlashPreparerContract $flash, ViewFactory $view)
     {
         $this->larabell = Container::getInstance()->make(Larabell::getFlashFacadeName());
         $this->flash    = $flash;
+        $this->view     = $view;
     }
 
     public function handle(Request $request, Closure $next)
     {
+        $this->bindToMessageBag($request);
+
         $this->larabell->load();
 
         $this->flash->handle($this->larabell, $request);
@@ -30,5 +42,20 @@ class LarabellMiddleware
         $this->larabell->save();
 
         return $response;
+    }
+
+    private function bindToMessageBag(Request $request)
+    {
+        // If the current session has a "messages" variable bound to it, we will share
+        // its value with all view instances so the views can easily access messages
+        // without having to bind. An empty collection is set when there aren't any messages.
+        $this->view->share(
+            'messages',
+            FlashViewMessageBag::make($request->session()->get(Larabell::getFlashFacadeName()) ?: [])
+        );
+
+        // Putting the messages in the view for every view allows the developer to just
+        // assume that some messages are always available, which is convenient since
+        // they don't have to continually run checks for the presence of messages.
     }
 }
