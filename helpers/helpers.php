1fb62768001b24b19d78de750e8e96598013ca8f
Imani Manyara <imani@simtabi.com>
2021-11-22 13:46:51 -0500

    Bug fixes

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -4,8 +4,8 @@ use Simtabi\Larabell\Flash\Flash\FlashNotifier;
 use Simtabi\Larabell\LarabelHelper;
 use Illuminate\Container\Container;
 
-if (!function_exists('larabellFlashNotifier')) {
-    function larabellFlashNotifier(...$args)
+if (!function_exists('larabellFlasher')) {
+    function larabellFlasher(...$args)
     {
         /** @var FlashNotifier $flash */
         $flash = Container::getInstance()->make(LarabelHelper::getFlashFacadeName());
